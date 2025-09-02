fa797432127761d4960952e92d0cf3201b930a07
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2025-09-02 21:10:49 +0530

    added requirnments and project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medicale_chatbot",
+    version="0.1.0",
+    author="Vipun Sajana",
+    author_email="vipunsanjana34@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
